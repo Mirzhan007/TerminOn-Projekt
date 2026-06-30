f107d23f7ebaa6fb93aec1813d46a9090052fe94
Mirzhan007 <ELD230761@spengergasse.at>
2026-06-30 17:39:30 +0200

    feat: Service-Klassen von Location & Doctor initialisiert.

---
+++ TerminOn/TerminOn.Application/Services/DoctorService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerminOn.Application.Infrastructur;
+using TerminOn.Application.Model;
+
+namespace TerminOn.Application.Services
+{
+    public class DoctorService
+    {
+        private readonly AppointmentContext _context;
+
+        public DoctorService(AppointmentContext context)
+        {
+            _context = context;
+        }
+
+        //stellt eine Liste von Ärzten da
+        public async Task<List<Doctor>> GetAllDoctorsAsync()
+        {
+            return await _context.Doctors.ToListAsync();
+        }
+
+        //Fügt einen neuen Arzt hinzu
+        public async Task AddDoctorAsync(Doctor doctor)
+        {
+            _context.Doctors.Add(doctor);
+            await _context.SaveChangesAsync();
+        }
+
+        //Ändert die Daten des Arztes
+        public async Task UpdateDoctorAsync(Doctor doctor)
+        {
+            _context.Doctors.Update(doctor);
+            await _context.SaveChangesAsync();
+        }
+
+
+        //Löscht einen Arzt von der Datenbank
+        public async Task DeleteDoctorAsync(int id)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+            if (doctor != null)
+            {
+                _context.Doctors.Remove(doctor);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ TerminOn/TerminOn.Application/Services/LocationService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerminOn.Application.Infrastructur;
+using TerminOn.Application.Model;
+
+namespace TerminOn.Application.Services
+{
+    public class LocationService
+    {
+        private readonly AppointmentContext _context;
+
+        public LocationService(AppointmentContext context)
+        {
+            _context = context;
+        }
+
+        // Holt alle Räume/Ordinationen für das Kalender-Grid
+        public async Task<List<Location>> GetAllLocationsAsync()
+        {
+            return await _context.Locations.ToListAsync();
+        }
+
+        // Fügt einen neuen Raum hinzu
+        public async Task AddLocationAsync(Location location)
+        {
+            _context.Locations.Add(location);
+            await _context.SaveChangesAsync();
+        }
+        //Ändert den Raum
+        public async Task UpdateLocationAsync(Location location)
+        {
+            _context.Locations.Update(location);
+            await _context.SaveChangesAsync();
+        }
+
+        //Löscht den Raum
+        public async Task DeleteLocationAsync(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location != null)
+            {
+                _context.Locations.Remove(location);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
